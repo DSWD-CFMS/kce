3183d444e2d14f03855398fea214088bd874c4bf
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-03-31 10:41:06 +0800

    asd

--- resources/views/user_rpmo/new_module.blade.php
+++ resources/views/user_rpmo/new_module.blade.php
@@ -7,42 +7,14 @@
 }
 
 @keyframes bounceIn {
-
-	from,
-	20%,
-	40%,
-	60%,
-	80%,
-	to {
-		animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-	}
-
-	0% {
-		opacity: 0;
-		transform: scale3d(0.3, 0.3, 0.3);
-	}
-
-	20% {
-		transform: scale3d(1.1, 1.1, 1.1);
-	}
-
-	40% {
-		transform: scale3d(0.9, 0.9, 0.9);
-	}
-
-	60% {
-		opacity: 1;
-		transform: scale3d(1.03, 1.03, 1.03);
-	}
-
-	80% {
-		transform: scale3d(0.97, 0.97, 0.97);
-	}
-
-	to {
-		opacity: 1;
-		transform: scale3d(1, 1, 1);
-	}
+	from,20%,40%,60%,80%,
+	to {animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);}
+	0% {opacity: 0;transform: scale3d(0.3, 0.3, 0.3);}
+	20% {transform: scale3d(1.1, 1.1, 1.1);}
+	40% {transform: scale3d(0.9, 0.9, 0.9);}
+	60% {opacity: 1;transform: scale3d(1.03, 1.03, 1.03);}
+	80% {transform: scale3d(0.97, 0.97, 0.97);}
+	to {opacity: 1;transform: scale3d(1, 1, 1);}
 }
 
 </style>
@@ -50,11 +22,11 @@
 <h4 class='text-center loading'><strong style="color:black;font-size: 20px;">Retrieving Data from the Server...</strong><br><br><i class="fa fa-database  fa-3x fa-fw" style="color:#86ebb8;font-size: 8x;"></i></h4>   
 </div>
 
-<div id="id01" class="modal">
-	<div class="modal-content">
-	<div class="container">
+<div id="id01" class="x-modal">
+	<div class="x-modal-content">
+	<div class="x-container">
 		<span onclick="$ID('id01').style.display='none'"
-		class="button display-topright">&times;</span>
+		class="x-button x-display-topright">&times;</span>
 		<p>Some text in the Modal..</p>
 		<p>Some text in the Modal..</p>
 	</div>
