1362397812ed1d503426a0f5a64896807182114a
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-03-31 11:38:43 +0800

    asd

--- app/Http/Controllers/RPMO_Controller.php
+++ app/Http/Controllers/RPMO_Controller.php
@@ -227,7 +227,7 @@ class RPMO_Controller extends Controller
     } 
     
     public function new_module_content_modal(Request $req){
-             $param1 = $req->data
+             $param1 = $req->data;
         // $results = DB::select( DB::raw("SELECT
         //         sp_brgy,
         //         sp.sp_id as sp_id,
