e237fd44be023558cca3949b082154e7c7bd9fda
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-04-05 10:31:54 +0800

    asd

--- resources/views/user_dac/subprojects.blade.php
+++ resources/views/user_dac/subprojects.blade.php
@@ -231,7 +231,7 @@
                         </div>
                         <div class="col-lg-4 hov">
                       <small class="text-label-blue"> <b>Target date of completion</b> </small>
-                          <p ng-if="specific_sp_data.sp[0].sp_target_date_of_completion != null" ng-bind="specific_sp_data.sp[0].sp_target_date_of_completion | date:'fullDate'"></p>
+                          <span ng-if="specific_sp_data.sp[0].sp_target_date_of_completion != null" ng-bind="specific_sp_data.sp[0].sp_target_date_of_completion | date:'fullDate'"></span>
                             <a href="" style="text-transform: none !important; font-size:1.1em; font-weight:bold;" class="text-warning" ng-click="updating_sp_data('sp_target_date_of_completion',specific_sp_data.sp[0].sp_id)"> <i class="fa fa-pencil-square-o"></i> Update </a>
                           <br>
                         </div>
