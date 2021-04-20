5ba33538acb4cfba6b6fddd9a7c43c51274bf963
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-04-20 16:26:16 +0800

    Update RPMO_Controller.php

--- app/Http/Controllers/RPMO_Controller.php
+++ app/Http/Controllers/RPMO_Controller.php
@@ -252,6 +252,7 @@ class RPMO_Controller extends Controller
         $query2 = Assigned_sp::where('sp_id', $req->id)->first();
         $query2->status = 'On-going';
         $query2->save();
+        echo "<script>alert(".$req->id.")</script>";
     } 
 
     public function iTextMoAPI($number,$message,$apicode,$passwd){
