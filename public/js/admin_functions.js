1d73817b61b9e841d79b47c465ea82bf371d2b3c
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-04-27 15:31:06 +0800

    Update admin_functions.js

--- public/js/admin_functions.js
+++ public/js/admin_functions.js
@@ -567,8 +567,7 @@ app.controller('Admin_Controller', function($scope,$http,$filter) {
       method : "GET",
       url : 'fetch_modality/'+modality_type+'/'+year,
     }).then(function mySuccess(response) {
-        console.log(response.data);
-        alert(response.data[0])
+        console.log(response.data[0]);
         $scope.sp_per_modality = response.data[0];
         $scope.sp_category = response.data[1];
         $scope.sp_type = response.data[2];
