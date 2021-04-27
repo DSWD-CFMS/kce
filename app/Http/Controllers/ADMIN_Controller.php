f737f27282fc7eaec5a630344008ec6989e93254
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-04-27 16:00:13 +0800

    Update ADMIN_Controller.php

--- app/Http/Controllers/ADMIN_Controller.php
+++ app/Http/Controllers/ADMIN_Controller.php
@@ -558,9 +558,9 @@ class ADMIN_Controller extends Controller
             INSERT IGNORE INTO `sp`
             (`sp_groupings`,`sp_id`, `sp_title`,`sp_province`, `sp_municipality`, `sp_brgy`, `sp_project_cost`, `sp_status`,`created_at`,`updated_at`,`remarks`)
             VALUES ('".$g_id."','".$rq["sp_id"]."','".$rq["sp_title"]."','".$rq["prov_name"]."','".$rq["city_name"]."','".$rq["brgy_name"]."','".$rq["grant"]."','NYS','".$rq["date_encoded"]."','".$rq["date_encoded"]."','"."FROM_CFMS_NEW_BTN');
-                 "
+                 ";
         // $users = DB::select( DB::raw($sqls));
-        return $sqls           
+        return $sqls;      
         // ob_end_flush();
     }
 
