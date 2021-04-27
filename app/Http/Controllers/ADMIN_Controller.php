ac748381be8dfbc261f7697870622c7af0c21df3
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-04-27 16:23:09 +0800

    Update ADMIN_Controller.php

--- app/Http/Controllers/ADMIN_Controller.php
+++ app/Http/Controllers/ADMIN_Controller.php
@@ -554,14 +554,13 @@ class ADMIN_Controller extends Controller
         ob_start('ob_gzhandler');
         $modality = array('kkb','makilahok','ncddp','ipcdd','ccl','lande','pamana','kc-af');
         $g_id = array_search(strtolower($rq['modality']), $modality)+1;
-        $sqls = "
+        $spsp = DB::select( DB::raw("
             INSERT IGNORE INTO `sp`
             (`sp_groupings`,`sp_id`, `sp_title`,`sp_province`, `sp_municipality`, `sp_brgy`, `sp_project_cost`, `sp_status`,`created_at`,`updated_at`,`remarks`)
             VALUES ('".$g_id."','".$rq->id."','".$rq->sp_title."','".$rq->prov_name."','".$rq->city_name."','".$rq->brgy_name."','".$rq->grant."','NYS','".$rq->date_encoded."','".$rq->date_encoded."','"."FROM_CFMS_NEW_BTN');
-                 ";
-        $users = DB::select( DB::raw($sqls));
+                 "));
 
-        return $sqls;      
+        return $spsp;      
         ob_end_flush();
     }
 
