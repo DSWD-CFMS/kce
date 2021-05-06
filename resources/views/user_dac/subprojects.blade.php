105fed08e31d0d98f81480b8d9aa1ec9b710b264
hasbrown019 <45794351+hashbrown019@users.noreply.github.com>
2021-05-06 14:39:26 +0800

    Update subprojects.blade.php

--- resources/views/user_dac/subprojects.blade.php
+++ resources/views/user_dac/subprojects.blade.php
@@ -64,8 +64,8 @@
 	<div class="table-responsive" style="height: 400px;">
 			<table class="table table-bordered1">
 				<tbody style="overflow-y: auto !important; overflow-x: none !important;">
-					<tr  ng-class="{'text-green' : all_data.status == 'Completed'}" style="font-size: 11px;" ng-repeat="all_data in bars = (my_modality.data | filter:search_data_modality) track by $index">
-						
+					<tr  ng-class="{'text-green' : all_data.status == 'Completed'}{'text-yellow' : all_data.status == 'On-going'}" style="font-size: 11px;" ng-repeat="all_data in bars = (my_modality.data | filter:search_data_modality) track by $index">
+
 						<td ng-class="{'text-green' : all_data.status == 'Completed'}" ng-bind="all_data.sp[0].sp_title"></td>
 						<td ng-class="{'text-green' : all_data.status == 'Completed'}" ng-bind="all_data.sp[0].sp_municipality | uppercase"></td>
 						<td ng-class="{'text-green' : all_data.status == 'Completed'}" ng-bind="all_data.sp[0].sp_brgy | uppercase"></td>
